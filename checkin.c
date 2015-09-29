66fa4563a399f36a097be5ec48e2c99fdbeb9a3a
Spencer McDonald <spencermcdonald@sandiego.edu>
2015-09-29 11:39:20 -0700

    Second Change

--- checkin.c
+++ checkin.c
@@ -4,7 +4,7 @@ int main() {
 
 	printf("COMP280 Roll Call:\n");
 	printf("1. Hello from Dr. Sat!\n");
-	printf("Hello from Spencer!\n");
+	printf("Hello from Spencer!!\n");
 	// Add your hello message below here.                                   
 	// Please give it a number (like above).                                
 	// Don't forget to end it with a newline ('\n') 
