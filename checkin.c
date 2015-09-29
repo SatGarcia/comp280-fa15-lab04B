0a2000383e00f47d4e4c0e7876b96f80d2ceff5f
Steven <steventran@sandiego.edu>
2015-09-29 11:37:58 -0700

    added checkin.c

--- checkin.c
+++ checkin.c
@@ -4,10 +4,11 @@ int main() {
 
 	printf("COMP280 Roll Call:\n");
 	printf("1. Hello from Dr. Sat!\n");
-
+        
 	// Add your hello message below here.                                   
 	// Please give it a number (like above).                                
 	// Don't forget to end it with a newline ('\n') 
 	
+	printf("2. Hello from Steven\n"); 
 	return 0;
 }
