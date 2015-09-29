816360a2076f2760863c73b096e877bd7dd1b2d5
Peter Gustafson <petergustafson19@yahoo.com>
2015-09-29 11:36:02 -0700

    added Hello From Peter

--- checkin.c
+++ checkin.c
@@ -4,6 +4,7 @@ int main() {
 
 	printf("COMP280 Roll Call:\n");
 	printf("1. Hello from Dr. Sat!\n");
+	printf("Hello From Peter\n");
 
 	// Add your hello message below here.                                   
 	// Please give it a number (like above).                                
